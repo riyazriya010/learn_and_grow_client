3a755e92d3511d9629bc5e0447f61993be7b1f23
riyazriya010 <riyur017@gmail.com.com>
2025-02-24 15:55:42 +0530

    socket updated

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,7 +1,8 @@
 // utils/socket.ts
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:8001', {
+// 'http://localhost:8001'
+const socket = io('https://api.learngrow.live', {
     transports: ["websocket", "polling"],
     withCredentials: true, // Ensure credentials are sent
 });
