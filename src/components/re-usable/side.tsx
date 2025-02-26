9405deb7ea1c674e880e592e807dcde996ce6651
riyazriya010 <riyur017@gmail.com.com>
2025-02-26 23:07:56 +0530

    side nav update

--- src/components/re-usable/side.tsx
+++ src/components/re-usable/side.tsx
@@ -9,24 +9,24 @@ interface SideNavProps {
 const SideNav: React.FC<SideNavProps> = ({ navLink }) => {
     return (
         <>
-        <nav 
-        className="flex flex-col bg-gradient-to-r from-white to-white-100 h-full min-h-screen w-64 p-4"
-        style={{ boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}
-        >
-            <ul className="space-y-4">
-                {navLink.map((item, index) => (
-                    <li
-                        key={index}
-                        className="text-lg font-small px-4 py-2 rounded-lg flex items-center gap-4 cursor-pointer hover:bg-[#22177A] hover:text-white"
-                    >
-                        {/* Add relevant icons */}
-                        {/* {index === 0 ? <FaSignInAlt /> : <FaUser />} */}
-                        <a href={item.link}>{item.field}</a>
-                    </li>
-                ))}
-            </ul>
-        </nav>
+            <nav
+                className="flex flex-col bg-gradient-to-r from-white to-white-100 h-auto w-64 p-4"
+                style={{ boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}
+            >
+                <ul className="space-y-4">
+                    {navLink.map((item, index) => (
+                        <li
+                            key={index}
+                            className="text-lg font-small px-4 py-2 rounded-lg flex items-center gap-4 cursor-pointer hover:bg-[#22177A] hover:text-white"
+                        >
+                            <a href={item.link}>{item.field}</a>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
         </>
+
+
     );
 };
 
