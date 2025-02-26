5db7f1879d95ab2af002ae5717cd72540e516e5e
riyazriya010 <riyur017@gmail.com.com>
2025-02-26 17:49:23 +0530

    new user profile update

--- src/components/re-usable/side.tsx
+++ src/components/re-usable/side.tsx
@@ -1,14 +1,26 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import { IoMenu } from "react-icons/io5";
+
 
 interface SideNavProps {
     navLink: { field: string; link: string }[];
 }
 
 const SideNav: React.FC<SideNavProps> = ({ navLink }) => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <>
+
+            {/* Hamburger button for small screens */}
+            <button
+                className="md:hidden p-2 fixed top-4 left-4 bg-[#22177A] text-white rounded-lg z-50"
+                onClick={() => setIsOpen(!isOpen)}
+            >
+                <IoMenu size={24} />
+            </button>
+
             <nav
                 className="hidden md:flex flex-col bg-gradient-to-r from-white to-white-100 h-full min-h-screen w-64 p-4 md:sticky top-0"
                 style={{ boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}
@@ -24,6 +36,14 @@ const SideNav: React.FC<SideNavProps> = ({ navLink }) => {
                     ))}
                 </ul>
             </nav>
+
+            {/* Overlay when menu is open (for mobile) */}
+            {isOpen && (
+                <div
+                    className="fixed inset-0 bg-black bg-opacity-50 md:hidden"
+                    onClick={() => setIsOpen(false)}
+                />
+            )}
 
         </>
     );
